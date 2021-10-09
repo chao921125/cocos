18f47775233b7005de747a17f78e8d30ff3f83ac
黄超 <chao.huang@advokategroup.com>
2021-10-09 15:09:10 +0800

    add

---
+++ assets/scripts/controller/LoginController.ts
@@ -0,0 +1,37 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    @property({ type: cc.Button })
+    playBtn: cc.Button | null = null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    onPlay() {
+        cc.director.loadScene("", this.onloadSceneFinish.bind(this));
+    }
+
+    onloadSceneFinish() {}
+
+    // update (dt) {}
+}
